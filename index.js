69173ad0dff1d816be57704efb48a4c9cac23ba1
arham80-commits <arram.tt.01@gmail.com>
2025-07-17 20:09:28 +0500

    ci setup

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
     }
 );
+app.get('/all', (req, res) => {
+    res.send('Hello, World! to earth');
+    }
+);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
